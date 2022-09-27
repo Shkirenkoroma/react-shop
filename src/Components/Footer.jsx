2e54f7a914fbd0c5b5d199171394eaa27227f6a0
romajusik <shkirenkoroma@mail.ru>
2022-09-27 12:22:26 +0300

    project complete

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="page-footer orange lighten-3">
+      <div className="footer-copyright">
+        <div className="container">
+          @ {new Date().getFullYear()} Shkirenko Roman
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
